87b91b3442a34459a97dcad741a07e3ebe404b32
Vibhu Garg <g.vibhu05@gmail.com>
2021-10-16 00:44:52 +0530

    Added level order traversal in Tree Node

--- TREE/TreeNode.java
+++ TREE/TreeNode.java
@@ -1,5 +1,9 @@
 package TREE;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class TreeNode {
     int val;
     TreeNode left;
@@ -7,5 +11,23 @@ public class TreeNode {
 
     TreeNode(int x) {
         val = x;
+    }
+
+    public void printLevelOrderTraversal(TreeNode root) {
+        if (null == root) {
+            return;
+        }
+        Queue<TreeNode> Q = new LinkedList<>();
+        Q.add(root);
+        while (!Q.isEmpty()) {
+            TreeNode node = Q.poll();
+            System.out.print(node.val + " ");
+            if (null != node.left) {
+                Q.add(node.left);
+            }
+            if (null != node.right) {
+                Q.add(node.right);
+            }
+        }
     }
 }
