1f86931ac8cf9bec6f0e81dc9c425225d9605345
Vibhu Garg <g.vibhu05@gmail.com>
2022-04-19 01:49:57 +0530

    [189] Rotate Array

---
+++ TOP_450/ARRAYS/RotateArray189.java
@@ -0,0 +1,52 @@
+package TOP_450.ARRAYS;
+
+import java.util.Arrays;
+
+/*
+  Problem-189: Given an array, rotate the array to the right by k steps, where k is non-negative.
+  Example 1:
+
+    Input: nums = [1,2,3,4,5,6,7], k = 3
+    Output: [5,6,7,1,2,3,4]
+    Explanation:
+        rotate 1 steps to the right: [7,1,2,3,4,5,6]
+        rotate 2 steps to the right: [6,7,1,2,3,4,5]
+        rotate 3 steps to the right: [5,6,7,1,2,3,4]
+
+    Constraints:
+
+    1 <= nums.length <= 105
+    -231 <= nums[i] <= 231 - 1
+    0 <= k <= 105
+
+    Solution: Observation
+    If you rotate the array k times, last k elements come at the front of the array.
+
+    Time Complexity :  O(N)
+    Space Complexity:  O(1)
+
+
+ */
+public class RotateArray189 {
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length; //if k>=nums.length,  after length times rotation, the array becomes same.
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    public void reverse(int[] nums, int start, int end) {
+        for (int i = start, j = end; i < j; i++, j--) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5, 6, 7};
+        RotateArray189 rotateArray189 = new RotateArray189();
+        rotateArray189.rotate(nums, 3);
+        Arrays.stream(nums).forEach(System.out::println);
+    }
+}
