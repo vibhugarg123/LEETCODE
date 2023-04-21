7be075bb615c6f8e904e76ab8de9d33a4ab6cac3
Vibhu Garg <g.vibhu05@gmail.com>
2023-04-21 21:19:35 +0530

    [85] Maximal Rectangle

--- TOP_450/MATRIX/MaximalRectangle85.java
+++ TOP_450/MATRIX/MaximalRectangle85.java
@@ -2,6 +2,20 @@ package TOP_450.MATRIX;
 
 import java.util.Stack;
 
+/*
+    Problem-85: https://leetcode.com/problems/maximal-rectangle/description/
+
+    Given a rows x cols binary matrix filled with 0's and 1's, find the largest rectangle containing only 1's and return its area.
+    Example 1:
+
+    Input: matrix = [["1","0","1","0","0"],
+                     ["1","0","1","1","1"],
+                     ["1","1","1","1","1"],
+                     ["1","0","0","1","0"]]
+
+
+    Output: 6
+ */
 public class MaximalRectangle85 {
     public int maximalRectangle(char[][] matrix) {
         int nRows = matrix.length;
